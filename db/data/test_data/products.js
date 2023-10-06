1eff370b6b7e25814287f40b2a86976a8cb03c52
nicola <you@example.com>
2023-10-06 13:49:17 +0100

    pics updated

--- db/data/test_data/products.js
+++ db/data/test_data/products.js
@@ -1,32 +1,42 @@
 module.exports = [
-    {
-      product_id:'price_1Nwo6FLSqeSGrFJihDSgcK47',
-      title: 'Lovely Vase',
-      topic: 'vases',
+  {product_id:'price_1Nwo5KLSqeSGrFJijG5Y7H1u',
+    title: 'large vase',
+    topic: 'vases',
+    author: 'Sammy',
+    body: 'This is a great handmade vase.',
+    created_at: 1604728980000,
+    likes: 0,
+    article_img_url:
+      'https://www.pexels.com/photo/a-brown-ceramic-pot-on-wooden-surface-8063822/',
+      more_images:'https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?w=700&h=700',
+      inventory:0,
+      price:40.00
+  },
+  {product_id:'price_1NxrvyLSqeSGrFJiOwYUsB4s',
+      title: 'Candle holder',
+      topic: 'tableware',
       author: 'Sammy',
-      body: 'This is a great handmade vase.',
+      body: 'This is a great candle holder.',
       created_at: 1604728980000,
       likes: 0,
       article_img_url:
-        'https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?w=700&h=700',
+        'https://www.pexels.com/photo/wax-candles-in-geometric-holders-near-mirror-7260283/',
         more_images:'https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?w=700&h=700',
         inventory:0,
-        price:40.00
+        price:10.00
     },
-    {
-      product_id:'price_1Nwo6FLSqeSGrFJihDSgcK47',
-        title: 'Lovely cup',
-        topic: 'tableware',
-        author: 'Sammy',
-        body: 'This is a great handmade cup.',
-        created_at: 1604728980000,
-        likes: 0,
-        article_img_url:
-          'https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?w=700&h=700',
-          more_images:'https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?w=700&h=700',
-          inventory:0,
-          price:15.00
-      },
-     
-    
-    ]
+    {product_id:'price_1Nwo6FLSqeSGrFJihDSgcK47',
+      title: 'Coffee Cup',
+      topic: 'tableware',
+      author: 'Sammy',
+      body: 'This is a great handmade cup.',
+      created_at: 1604728980000,
+      likes: 0,
+      article_img_url:
+        'https://www.pexels.com/photo/ceramic-cups-placed-on-white-shelf-6310185/',
+        more_images:'https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?w=700&h=700',
+        inventory:0,
+        price:15.00
+    },
+  
+  ]
